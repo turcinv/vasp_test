7265e67df3ed09e1ade149caf6116276e6db748e
turcinv <vitturcin@gmail.com>
2025-02-22 18:35:55 +0100

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='vasp_tools',
     packages=find_packages(),
-    version='0.0.3',
+    version='0.0.4',
     description='Vasp tools for analysis and visualization',
     author='Vit Turcin',
 )
