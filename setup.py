02003bdf6b2c4a7d34b70c0f3f34fac4445328ef
turcinv <vitturcin@gmail.com>
2025-04-03 09:49:16 +0200

    ...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='vasp_tools',
     packages=find_packages(),
-    version='1.0.0',
+    version='1.0.1',
     description='Vasp tools for analysis',
     author='Vit Turcin',
 )
